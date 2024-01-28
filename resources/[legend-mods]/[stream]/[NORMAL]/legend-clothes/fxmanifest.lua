04a409ad5e7d9de8d04a06d8b6ef824d6c45b455
Gabriel <bielzinhofrau@gmail.com>
2024-01-28 07:33:41 -0800

    ATUALIZAÇÃO - 28/01/2024
    STREAM / SCRIPT

---
+++ resources/[legend-mods]/[stream]/[NORMAL]/legend-clothes/fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'peds.meta',
+  'mp_m_freemode_01_mp_m_clothes2.meta',
+  'mp_f_freemode_01_mp_f_clothes2.meta',
+  'mp_m_freemode_01_mp_m_clothes.meta',
+  'mp_f_freemode_01_mp_f_clothes.meta',
+  'mp_f_freemode_01_mp_f_female_police.meta',
+  'mp_m_freemode_01_mp_m_male_police.meta',
+  'mp_m_freemode_01_mecanica.meta',
+  'mp_f_freemode_01_mecanica.meta',
+  'mp_m_freemode_01_fac.meta',
+  'mp_f_freemode_01_fac.meta',
+}
+
+data_file 'PED_METADATA_FILE' 'peds.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_clothes2.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_clothes2.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_clothes.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_clothes.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_female_police.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_male_police.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mecanica.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mecanica.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_fac.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_fac.meta'
---
+++ resources/[legend-mods]/[stream]/[NORMAL]/legend-galaxy/fxmanifest.lua
@@ -0,0 +1,16 @@
+-- shared_script "@vrp/lib/lib.lua" --Para remover esta pendencia de todos scripts, execute no console o comando "uninstall"
+
+
+
+fx_version 'cerulean'
+games {'gta5'}
+
+this_is_a_map 'yes'
+
+Author 'Rfc_Mapping'
+description 'East Customs Garage'
+
+version '3.0'
+
+                            
+dependency '/assetpacks'
